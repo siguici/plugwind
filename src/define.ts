52a2b2ebef98b91be7bd5b7005c2fe1e9a0d104b
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-09-16 15:11:08 +0100

    🐛 Fix theme

--- src/define.ts
+++ src/define.ts
@@ -95,6 +95,56 @@ function getData(path: string | undefined, data: any, defaultValue: any): any {
   return value ?? defaultValue;
 }
 
+function normalizeValue(value: any): any {
+  if (typeof value === 'function') {
+    return value();
+  }
+  if (Array.isArray(value)) {
+    return value.map(normalizeValue).join(', ');
+  }
+
+  return value;
+}
+
+function normalizeTheme(config: ThemeConfig): Record<string, any> {
+  const base: Record<string, any> = {};
+  const extensions: Record<string, any> = {};
+
+  for (let [key, value] of Object.entries(config)) {
+    if (key === 'extend') {
+      Object.assign(extensions, value);
+    } else {
+      base[key] = value;
+    }
+  }
+
+  for (let [key, value] of Object.entries(extensions)) {
+    if (typeof base[key] === 'object' && base[key] !== null) {
+      base[key] = {
+        ...base[key],
+        ...value,
+      };
+    } else {
+      base[key] = value;
+    }
+  }
+
+  const theme: Record<string, any> = {};
+  for (let [key, value] of Object.entries(base)) {
+    const prefix = `--${camelToSnake(key, '-')}`;
+    if (typeof value === 'object' && value) {
+      for (let [subkey, subvalue] of Object.entries(value)) {
+        theme[`${prefix}-${camelToSnake(subkey, '-')}`] =
+          normalizeValue(subvalue);
+      }
+    } else {
+      theme[prefix] = normalizeValue(value);
+    }
+  }
+
+  return theme;
+}
+
 export function renderCss(stmts: CssStmts): Css {
   return JSON.stringify(stmts, undefined, 4);
 }
@@ -109,18 +159,7 @@ export function definePlugin(plugin: Plugin, config?: UserConfig): CssStmts {
     stmts.push(head);
   }
   if (config?.theme) {
-    const theme: Record<string, any> = {};
-    for (let [key, value] of Object.entries(config.theme)) {
-      key = `--${camelToSnake(key, '-')}`;
-      if (typeof value === 'object' && value) {
-        for (let [subkey, subvalue] of Object.entries(value)) {
-          theme[`${key}-${camelToSnake(subkey, '-')}`] = subvalue;
-        }
-      } else {
-        theme[key] = value;
-      }
-    }
-    stmts.push({ '@theme': theme });
+    stmts.push({ '@theme': normalizeTheme(config.theme) });
   }
   let prefix = config?.prefix || '';
   const api: PluginAPI = extendAPI(() => ({
